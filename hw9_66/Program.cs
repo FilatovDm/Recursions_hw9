3c0d4b3ed3f735b17decb50f4f8271ee83e7796a
Dmitriy Filatov <filatov-d@mail.ru>
2022-09-29 16:10:18 +0300

    Task 66 completed

---
+++ hw9_66/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetNaturalNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        int sum = m;
+
+        if (m != n)
+        {
+            sum = sum + GetNaturalNumbers(m + 1, n);
+        }
+        
+    }
+    else if (m >= n)
+    {
+        int sum = n;
+
+        if (n < m)
+        {
+            sum = sum + GetNaturalNumbers(m, n + 1);
+        }
+        
+    }
+    return sum;
+}
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sumElem = GetNaturalNumbers(m, n);
+System.Console.WriteLine($"Сумма элементов от {m} до {n}: {sumElem}");
